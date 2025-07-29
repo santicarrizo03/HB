c3245ad8c09795c34778d185ac1be37b30709d1e
santi carrizo <santiagocarrizo53@gmail.com>
2025-07-29 10:37:20 -0300

    3

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var w = c.width = window.innerWidth,
 		hh = h / 2,
 		
 		opts = {
-			strings: [ 'HAPPY', 'BIRTHDAY!', 'PICHI'],
+			strings: [ 'FELIZ', 'CUMPLE', 'PICHI!'],
 			charSize: 30,
 			charSpacing: 35,
 			lineHeight: 40,
